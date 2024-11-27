ca8f4a4f6e7082757488d94914d3df041a1d46e7
moritzgauss <moritzgg99@gmail.com>
2024-11-27 12:29:02 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+<script>
+    document.querySelectorAll(".folder-item > .folder-icon").forEach((icon) => {
+        icon.addEventListener("click", function () {
+            const parent = this.parentNode;
+            parent.classList.toggle("open");
+            const subfolder = parent.querySelector(".subfolder");
+            if (subfolder) {
+                subfolder.style.display = parent.classList.contains("open") ? "block" : "none";
+                this.textContent = parent.classList.contains("open") ? "📂" : "🗂️";
+            }
+        });
+    });
+</script>
