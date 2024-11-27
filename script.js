40ea68fc2f8d6f00cc616e4dd9b62133d5906f36
moritzgauss <moritzgg99@gmail.com>
2024-11-27 14:39:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-<script>
-    document.querySelectorAll(".folder-item > .folder-icon").forEach((icon) => {
-        icon.addEventListener("click", function () {
-            const parent = this.parentNode;
-            parent.classList.toggle("open");
-            const subfolder = parent.querySelector(".subfolder");
-            if (subfolder) {
-                subfolder.style.display = parent.classList.contains("open") ? "block" : "none";
-                this.textContent = parent.classList.contains("open") ? "📂beach.mori77z.de" : "📁beach.mori77z.de";
-            }
+        document.querySelectorAll(".folder-item > .folder-icon").forEach((icon) => {
+            icon.addEventListener("click", function () {
+                const parent = this.parentNode;
+                parent.classList.toggle("open");
+                const subfolder = parent.querySelector(".subfolder");
+                if (subfolder) {
+                    subfolder.style.display = parent.classList.contains("open") ? "block" : "none";
+                    this.textContent = parent.classList.contains("open") ? "📂 beach.mori77z.de" : "📁 beach.mori77z.de";
+                }
+            });
         });
-    });
-</script>
